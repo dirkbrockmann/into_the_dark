e241c25938d483672bcb9c1b86aa8cfc00e4457e
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-12-28 14:35:05 +0100

    v1.1.1

--- src/meta.js
+++ src/meta.js
@@ -1,5 +1,5 @@
 export default {
-  "title": "Barista's Secret",
-  "subtitle" : "Percolation on a lattice",
-  "description": "This explorable illustrates the phenomenon of percolation, when e.g. a liquid like water moves through a porous medium, like coffee. The percolation process is a critical phenomenon that exhibits interesting properties at the critical porosity of the medium."
+  "title": "Into the Dark",
+  "subtitle" : "Collective intelligence",
+  "description": "This explorable illustrates the emergence of collective intelligence in a model for a school of fish. The combination of speed differences in the light and the dark and cohesive swarm forces helps the swarm find the safe regions."
 }
